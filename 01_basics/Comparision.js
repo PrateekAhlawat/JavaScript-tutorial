76a1c4b0f906978e157e6c6fdba9f960dd46aca8
PrateekAhlawat <prateekahlawat30@gmail.com>
2026-01-25 08:41:44 +0000

    Comparision.js

---
+++ 01_basics/Comparision.js
@@ -0,0 +1,19 @@
+  /* COMPARISION */
+
+// Comparions :- 
+//  console.log(2 > 1)   // true
+//  console.log(2 < 1)   // false
+//  console.log(2 == 1)  // false
+//  console.log(2 >= 1)  // true
+//  console.log(2 <= 1)  // false
+//  console.log(2 != 1)  // ture
+
+// // Comapre with different datatypes:-
+//  console.log("2" > 1)  // true
+//  console.log("07" < 10) //true
+//  console.log(null > 0)  // false
+//  console.log(null == 0)  // false
+//  console.log(null >= 0)  // true
+
+// check and compare datatypes (===):-
+ console.log("02" === 2)  //false :-(string != Number)
